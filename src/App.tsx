1a81f795b5cb5d4af0e847ff1af75032d8cd6ec4
Vidarshan <vidarshanadithya3@gmail.com>
2022-03-26 11:56:34 +0530

    Finalized basic grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,113 +1,307 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Container, Grid, Text, Col } from "@mantine/core";
+import { Container, Grid, Text, Col, Box, Image } from "@mantine/core";
+import castle from "./images/castle.png";
+import robot from "./images/robot.png";
 
 const App = () => {
+  const gridSizeInput = 5;
+  const [gridSize, setGridSize] = useState(0);
+
+  const calculateGridSize = (sizeInput: number) => {
+    setGridSize(sizeInput * sizeInput);
+  };
+
+  const returnGrid = () => {
+    for (let i = 0; i <= gridSize; i++) {
+      return <Box>dgdgd</Box>;
+    }
+  };
+
+  useEffect(() => {
+    calculateGridSize(gridSizeInput);
+  }, []);
+
   return (
-    <Container
+    <Box
       sx={{
         backgroundColor: "red",
         height: "100vh",
-        // display: "flex",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
       }}
-      fluid
     >
-      <Grid
-        sx={{
-          display: "flex",
-          backgroundColor: "honeydew",
-          justifyContent: "center",
-          height: "100vh",
-          maxWidth: "960px",
-          padding: "4rem",
-        }}
-      >
-        <Col
-          sx={{
-            backgroundColor: "beige",
-            margin: "5px",
-            maxHeight: "100px",
-            maxWidth: "100px",
-          }}
-          span={2}
-        >
-          <Text>f</Text>
-        </Col>
-        <Col sx={{ backgroundColor: "beige", margin: "5px" }} span={2}>
-          <Text>f</Text>
-        </Col>
-        <Col sx={{ backgroundColor: "beige", margin: "5px" }} span={2}>
-          <Text>f</Text>
-        </Col>
-        <Col sx={{ backgroundColor: "beige", margin: "5px" }} span={2}>
-          <Text>f</Text>
-        </Col>
-        <Col sx={{ backgroundColor: "beige", margin: "5px" }} span={2}>
-          <Text>f</Text>
-        </Col>
-        <Col sx={{ backgroundColor: "beige", margin: "5px" }} span={2}>
-          <Text>f</Text>
-        </Col>
-        <Col sx={{ backgroundColor: "beige", margin: "5px" }} span={2}>
-          <Text>f</Text>
-        </Col>
-        <Col sx={{ backgroundColor: "beige", margin: "5px" }} span={2}>
-          <Text>f</Text>
-        </Col>
-        <Col sx={{ backgroundColor: "beige", margin: "5px" }} span={2}>
-          <Text>f</Text>
-        </Col>
-        <Col sx={{ backgroundColor: "beige", margin: "5px" }} span={2}>
-          <Text>f</Text>
-        </Col>
-        <Col sx={{ backgroundColor: "beige", margin: "5px" }} span={2}>
-          <Text>f</Text>
-        </Col>
-        <Col sx={{ backgroundColor: "beige", margin: "5px" }} span={2}>
-          <Text>f</Text>
-        </Col>
-        <Col sx={{ backgroundColor: "beige", margin: "5px" }} span={2}>
-          <Text>f</Text>
-        </Col>
-        <Col sx={{ backgroundColor: "beige", margin: "5px" }} span={2}>
-          <Text>f</Text>
-        </Col>
-        <Col sx={{ backgroundColor: "beige", margin: "5px" }} span={2}>
-          <Text>f</Text>
-        </Col>
-        <Col sx={{ backgroundColor: "beige", margin: "5px" }} span={2}>
-          <Text>f</Text>
-        </Col>
-        <Col sx={{ backgroundColor: "beige", margin: "5px" }} span={2}>
-          <Text>f</Text>
-        </Col>
-        <Col sx={{ backgroundColor: "beige", margin: "5px" }} span={2}>
-          <Text>f</Text>
-        </Col>
-        <Col sx={{ backgroundColor: "beige", margin: "5px" }} span={2}>
-          <Text>f</Text>
-        </Col>
-        <Col sx={{ backgroundColor: "beige", margin: "5px" }} span={2}>
-          <Text>f</Text>
-        </Col>
-        <Col sx={{ backgroundColor: "beige", margin: "5px" }} span={2}>
-          <Text>f</Text>
-        </Col>
-        <Col sx={{ backgroundColor: "beige", margin: "5px" }} span={2}>
-          <Text>f</Text>
-        </Col>
-        <Col sx={{ backgroundColor: "beige", margin: "5px" }} span={2}>
-          <Text>f</Text>
-        </Col>
-        <Col sx={{ backgroundColor: "beige", margin: "5px" }} span={2}>
-          <Text>f</Text>
-        </Col>
-        <Col sx={{ backgroundColor: "beige", margin: "5px" }} span={2}>
-          <Text>f</Text>
-        </Col>
-      </Grid>
-    </Container>
+      <Box sx={{ backgroundColor: "greenyellow" }}>
+        <Container sx={{ display: "flex", flexDirection: "row" }}>
+          <Box
+            sx={{
+              backgroundColor: "beige",
+              height: "50px",
+              width: "50px",
+              margin: "10px",
+            }}
+          >
+            f
+          </Box>
+          <Box
+            sx={{
+              backgroundColor: "beige",
+              height: "50px",
+              width: "50px",
+              margin: "10px",
+            }}
+          >
+            f
+          </Box>
+          <Box
+            sx={{
+              backgroundColor: "beige",
+              height: "50px",
+              width: "50px",
+              margin: "10px",
+            }}
+          >
+            f
+          </Box>
+          <Box
+            sx={{
+              backgroundColor: "beige",
+              height: "50px",
+              width: "50px",
+              margin: "10px",
+            }}
+          >
+            f
+          </Box>
+          <Box
+            sx={{
+              backgroundColor: "beige",
+              height: "50px",
+              width: "50px",
+              margin: "10px",
+            }}
+          >
+            f
+          </Box>
+        </Container>
+
+        <Container sx={{ display: "flex", flexDirection: "row" }}>
+          <Box
+            sx={{
+              backgroundColor: "beige",
+              height: "50px",
+              width: "50px",
+              margin: "10px",
+            }}
+          >
+            f
+          </Box>
+          <Box
+            sx={{
+              backgroundColor: "beige",
+              height: "50px",
+              width: "50px",
+              margin: "10px",
+            }}
+          >
+            f
+          </Box>
+          <Box
+            sx={{
+              backgroundColor: "beige",
+              height: "50px",
+              width: "50px",
+              margin: "10px",
+            }}
+          >
+            f
+          </Box>
+          <Box
+            sx={{
+              backgroundColor: "beige",
+              height: "50px",
+              width: "50px",
+              margin: "10px",
+            }}
+          >
+            f
+          </Box>
+          <Box
+            sx={{
+              backgroundColor: "beige",
+              height: "50px",
+              width: "50px",
+              margin: "10px",
+            }}
+          >
+            f
+          </Box>
+        </Container>
+
+        <Container sx={{ display: "flex", flexDirection: "row" }}>
+          <Box
+            sx={{
+              backgroundColor: "beige",
+              height: "50px",
+              width: "50px",
+              margin: "10px",
+            }}
+          >
+            f
+          </Box>
+          <Box
+            sx={{
+              backgroundColor: "beige",
+              height: "50px",
+              width: "50px",
+              margin: "10px",
+            }}
+          >
+            f
+          </Box>
+          <Box
+            sx={{
+              backgroundColor: "beige",
+              height: "50px",
+              width: "50px",
+              margin: "10px",
+            }}
+          >
+            <Image src="" />
+          </Box>
+          <Box
+            sx={{
+              backgroundColor: "beige",
+              height: "50px",
+              width: "50px",
+              margin: "10px",
+            }}
+          >
+            f
+          </Box>
+          <Box
+            sx={{
+              backgroundColor: "beige",
+              height: "50px",
+              width: "50px",
+              margin: "10px",
+            }}
+          >
+            f
+          </Box>
+        </Container>
+
+        <Container sx={{ display: "flex", flexDirection: "row" }}>
+          <Box
+            sx={{
+              backgroundColor: "beige",
+              height: "50px",
+              width: "50px",
+              margin: "10px",
+            }}
+          >
+            f
+          </Box>
+          <Box
+            sx={{
+              backgroundColor: "beige",
+              height: "50px",
+              width: "50px",
+              margin: "10px",
+            }}
+          >
+            f
+          </Box>
+          <Box
+            sx={{
+              backgroundColor: "beige",
+              height: "50px",
+              width: "50px",
+              margin: "10px",
+            }}
+          >
+            f
+          </Box>
+          <Box
+            sx={{
+              backgroundColor: "beige",
+              height: "50px",
+              width: "50px",
+              margin: "10px",
+            }}
+          >
+            f
+          </Box>
+          <Box
+            sx={{
+              backgroundColor: "beige",
+              height: "50px",
+              width: "50px",
+              margin: "10px",
+            }}
+          >
+            f
+          </Box>
+        </Container>
+
+        <Container sx={{ display: "flex", flexDirection: "row" }}>
+          <Box
+            sx={{
+              backgroundColor: "beige",
+              height: "50px",
+              width: "50px",
+              margin: "10px",
+            }}
+          >
+            f
+          </Box>
+          <Box
+            sx={{
+              backgroundColor: "beige",
+              height: "50px",
+              width: "50px",
+              margin: "10px",
+            }}
+          >
+            f
+          </Box>
+          <Box
+            sx={{
+              backgroundColor: "beige",
+              height: "50px",
+              width: "50px",
+              margin: "10px",
+            }}
+          >
+            f
+          </Box>
+          <Box
+            sx={{
+              backgroundColor: "beige",
+              height: "50px",
+              width: "50px",
+              margin: "10px",
+            }}
+          >
+            f
+          </Box>
+          <Box
+            sx={{
+              backgroundColor: "beige",
+              height: "50px",
+              width: "50px",
+              margin: "10px",
+            }}
+          >
+            f
+          </Box>
+        </Container>
+      </Box>
+      {/* {returnGrid()} */}
+    </Box>
   );
 };
 
